ef22a3b2571722deff6ee9b2ccf018364a732e6b
Victor Hugo Vianna Silva <victorvianna@google.com>
2024-11-12 11:10:37 +0000

    commit

---
+++ server.py
@@ -0,0 +1,12 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b'Hello, world!')
+
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
